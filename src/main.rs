7174c263f525246849cfdc87e63993d42a64e334
Kevin Durbin <kevindurb@gmail.com>
2020-06-25 07:46:56 -0600

    make it work again

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,27 @@ extern crate gtk;
 use gio::prelude::*;
 use gtk::prelude::*;
 
-use gtk::{Application, ApplicationWindow, Button, Scale};
-
 fn main() {
     let application =
-        Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
             .expect("failed to initialize GTK application");
 
     application.connect_activate(|app| {
-        let window = ApplicationWindow::new(app);
+        let window = gtk::ApplicationWindow::new(app);
         window.set_title("First GTK+ Program");
         window.set_default_size(350, 70);
 
         let container = gtk::Box::new(gtk::Orientation::Vertical, 16);
 
-        let button = Button::new_with_label("Click me!");
+        let button = gtk::Button::new_with_label("Click me!");
         button.connect_clicked(|_| {
             println!("Clicked!");
         });
         container.add(&button);
 
-        let scale = Scale::new_with_range(gtk::Orientation::Horizontal, 0.0, 10.0, 1.0);
+        let scale = gtk::Scale::new_with_range(gtk::Orientation::Horizontal, 0.0, 10.0, 1.0);
         scale.connect_value_changed(|_| {
-            println!("new value {}", scale.getValue());
+            println!("scale changed");
         });
         container.add(&scale);
 
